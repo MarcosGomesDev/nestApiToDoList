80f1716decf0ff7ab04024be096e807b30c7d038
Marcos Gomes <marcosgomesdev@gmail.com>
2023-09-03 11:13:48 -0300

    base route / isPublic

--- src/application/controllers/app.controller.ts
+++ src/application/controllers/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from '../services/app.service';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
